e12644d662965dd67e0aaed19b90aa39b9ead398
rajat kapoor <rajatkapoor214@gmail.com>
2022-04-23 07:07:13 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header/>
+      <main>
+        <h1>Welocome</h1>
+      </main>
+      <Footer/>
     </>
   );
 }
